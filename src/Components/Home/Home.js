b3d2f676c19a76148d90db4eb3c26477af2c50f4
unknown <necmeddinxelilzade2002@gmail.com>
2024-12-18 00:22:03 +0400

    change

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,56 +1,84 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./home.module.css";
-import img from "../../Assets/images/homepage.jpg";
+import productimage from "../../Assets/images/product_icon.png";
 import one from "../../Assets/images/one.pnj.png";
 
 const Home = () => {
-    return (
-        <div>
-
-            <div>
-                <div>The Most Sought-After Product Management and Product Marketing Certifications In The World</div>
-                <div className={styles.cardcontainer}>
-                   <div className={styles.card}>
-                      <span className={styles.carttext}>Product Manager</span>
-                      <span>Learn More </span>
-                      <div><img className={styles.imageone} src={one} alt="" /></div>
-                   </div>
-                   <div className={styles.card}>
-                      <span>Product Manager</span>
-                      <span>Learn More </span>
-                      <div><img className={styles.imageone} src={one} alt="" /></div>
-                   </div>
-                   <div className={styles.card}>
-                      <span>Product Manager</span>
-                      <span>Learn More </span>
-                      <div><img className={styles.imageone} src={one} alt="" /></div>
-                   </div>
-
-                 
-                </div>
-                <div  className={styles.cardcontainer}>
-                <div className={styles.card}>
-                      <span>Product Manager</span>
-                      <span>Learn More </span>
-                      <div><img className={styles.imageone} src={one} alt="" /></div>
-                   </div>
-                   <div className={styles.card}>
-                      <span>Product Manager</span>
-                      <span>Learn More </span>
-                      <div><img className={styles.imageone} src={one} alt="" /></div>
-                   </div>
-                  
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div>
+        <div className={styles.textline} >
+          The Most Sought-After Product Management and Product Marketing
+          Certifications In The World
         </div>
-    );
-}
+        <div className={styles.cardcontainer}>
+          <div className={styles.card}>
+            {" "}
+            <div>
+              <img className={styles.imageone} src={one} alt="" />
+            </div>
+            <div className={styles.carttext}>
+              <span>Product Manager</span>
+              <span>Learn More </span>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div>
+              <img className={styles.imageone} src={one} alt="" />
+            </div>
+            <div className={styles.carttext}>
+              <span>Product Manager</span>
+              <span>Learn More </span>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div>
+              <img className={styles.imageone} src={one} alt="" />
+            </div>
+            <div className={styles.carttext}>
+              <span>Product Manager</span>
+              <span>Learn More </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardcontainer}>
+          <div className={styles.card}>
+            <div>
+              <img className={styles.imageone} src={one} alt="" />
+            </div>
+            <div className={styles.carttext}>
+              <span>Product Manager</span>
+              <span>Learn More </span>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div>
+              <img className={styles.imageone} src={one} alt="" />
+            </div>
+            <div className={styles.carttext}>
+              <span>Product Manager</span>
+              <span>Learn More </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.boxline} >
+         <div className={styles.box}>View Certifications</div>
+         <div className={styles.box}>Viey Courses</div>
+        </div>
+      </div>
+      <div>
+        {/* <div>
+          <img className={styles.cartboximage} src={productimage} alt="" />
+          <h1>Product</h1>
+          <span>Equip your team to create outstanding product</span>
+          <span>Learn More</span>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
 
-
-
-// cardtext
-// className={styles.carttext} 
+// className={styles.carttext}
